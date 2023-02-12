e0ca9c1b887e10c92c998db10334c90f9a9c93fd
CStreete <112933415+CStreete@users.noreply.github.com>
2023-02-12 22:07:36 +0100

    final version

--- src/main.js
+++ src/main.js
@@ -16,25 +16,9 @@ const updateUserOption = document.getElementById('updateUser');
 
 
 
-function choiceFunc (){
-
-    switch(choice.value){
-        case viewOrdersOption.value:
-            viewOrders();
-            break;
-        case deleteUserOption.value:
-            renderDeleteUserHTML();
-            break;
-        case updateUserOption.value:
-            renderUpdateUserHTML();
-            break;
-    }
-
-
-    
+window.onload = ()=>{
+    viewOrders();
 }
-
-myCbtn.addEventListener('click', choiceFunc);
 
 
 
@@ -47,15 +31,14 @@ fetch('https://firestore.googleapis.com/v1/projects/fir-demo-99463/databases/(de
 .then(res=> res.json())
 .then(data => {
     const appData = data.documents;
-    
     console.log(appData)
 
     for (const item of appData) {
         let documentID = item.name;
         let lastString = documentID.substr(documentID.length - 20)
-        
+       
         form.innerHTML += ` 
-        <article class="text-lg text-black my-5 flex bg-white justify-center w-fit px-14 py-14 shadow-black shadow-md">
+        <article  id="article" class="text-lg text-black my-5 flex flex-col  bg-white justify-center w-fit px-14 py-14 shadow-black shadow-md">
         <ul class="">
             <li>
                 <span class=" font-semibold"> Order ID: </span>
@@ -74,6 +57,10 @@ fetch('https://firestore.googleapis.com/v1/projects/fir-demo-99463/databases/(de
             ${item.fields.number.stringValue}
             </li>
             <li>
+            <span class=" font-semibold"> Order Items: </span>
+            ${item.fields.orderItems.stringValue}
+            </li>
+            <li>
             <span class=" font-semibold"> Shipping Method: </span>
             ${item.fields.shippingMethod.stringValue}
             </li>
@@ -89,12 +76,14 @@ fetch('https://firestore.googleapis.com/v1/projects/fir-demo-99463/databases/(de
             <span class=" font-semibold"> Postal Code: </span>
             ${item.fields.shippingAdress.mapValue.fields.postalCode.stringValue}
             </li>
-        
-           
          </ul>
+         <div class=" pl-8 flex w-full">
+         <input type="button" id="deleteBtn" value="Delete Order" onclick="deleteUser('${lastString}')"  class=" w-40 mt-5 mr-5 h-9 placeholder: pl-2 rounded-md bg-fancyBlack text-white text-center">
+         <input type="button" id="updateBtn" value="Update Order" onclick="renderUpdateUserHTML('${lastString}')" class=" w-40 mt-5 h-9 placeholder: pl-2 rounded-md bg-fancyBlack text-white text-center">
+         <div>
         </article>
     `
-    
+        
     }
 
 
@@ -104,150 +93,199 @@ fetch('https://firestore.googleapis.com/v1/projects/fir-demo-99463/databases/(de
 }
 
 
-function renderDeleteUserHTML (){
-    return form.innerHTML = 
-    `
-    <form class="flex flex-col items-center">
-        
-        <input type="text" id="id" placeholder="Enter Id" class="text-white  rounded-md bg-fancyBlack w-96 mt-5 h-9 placeholder: pl-2 placeholder:text-white">
-        <input type="button" id="deleteBtn" value="Delete User" onclick="deleteUser()"  class=" w-40 mt-5 h-9 placeholder: pl-2 rounded-md bg-fancyBlack text-white text-center">
-    </form>
-    `
+
+function renderUpdateUserHTML(orderID){
+
+    fetch("https://firestore.googleapis.com/v1/projects/fir-demo-99463/databases/(default)/documents/orders/" + orderID)
+            .then(res=> res.json())
+            .then(data=> {
+                const appData = data;
+                const article = document.getElementById('article');
+
+                article.innerHTML = ` 
+                
+                   
+                    <span class=" font-semibold"> Order ID: ${orderID} </span>
+                    <form class="flex flex-col items-center">
+                        <input type="text" id="name" name="name" placeholder="${appData.fields.name.stringValue}" class="text-white  rounded-md bg-fancyBlack w-96 mt-5 h-9 placeholder: pl-2 placeholder:text-white">
+                        <input type="text" id="email" name="email" placeholder="${appData.fields.email.stringValue}" class="text-white  rounded-md bg-fancyBlack w-96 mt-5 h-9 placeholder: pl-2 placeholder:text-white">
+                        <input type="text" id="tele" name="tele" placeholder="${appData.fields.number.stringValue}" class="text-white  rounded-md bg-fancyBlack w-96 mt-5 h-9 placeholder: pl-2 placeholder:text-white">
+                        <input type="text" id="orderItems" name="orderItems" placeholder="${appData.fields.orderItems.stringValue}" class="text-white  rounded-md bg-fancyBlack w-96 mt-5 h-9 placeholder: pl-2 placeholder:text-white">
+                        <input type="text" id="shipping-method" name="shipping-method" placeholder="${appData.fields.shippingMethod.stringValue}" class="text-white  rounded-md bg-fancyBlack w-96 mt-5 h-9 placeholder: pl-2 placeholder:text-white">
+                        <input type="text" id="adress" name="adress" placeholder="${appData.fields.shippingAdress.mapValue.fields.adress.stringValue}" class="text-white  rounded-md bg-fancyBlack w-96 mt-5 h-9 placeholder: pl-2 placeholder:text-white">
+                        <input type="text" id="city" name="city" placeholder="${appData.fields.shippingAdress.mapValue.fields.city.stringValue}" class="text-white  rounded-md bg-fancyBlack w-96 mt-5 h-9 placeholder: pl-2 placeholder:text-white">
+                        <input type="text" id="postal-code" name="postal-code" placeholder="${appData.fields.shippingAdress.mapValue.fields.postalCode.stringValue}" class="text-white  rounded-md bg-fancyBlack w-96 mt-5 h-9 placeholder: pl-2 placeholder:text-white">
+                        <input type="button" id="updateBtn" value="Update User" onclick="updateUser('${orderID}')" class=" w-40 mt-5 h-9 placeholder: pl-2 rounded-md bg-fancyBlack text-white text-center">
+                        <input type="button" id="cancelBtn" value="Cancel" onclick="location.reload()" class=" w-40 mt-5 h-9 placeholder: pl-2 rounded-md bg-fancyBlack text-white text-center">
+                     </form>               
+              `
+             
+            })
 }
 
 
-function renderUpdateUserHTML(){
-    return form.innerHTML = 
-    ` 
-    <form class="flex flex-col items-center">
-        <input type="text" id="id" placeholder="Enter Id (Requierd)" name="id" class="text-white  rounded-md bg-fancyBlack w-96 mt-5 h-9 placeholder: pl-2 placeholder:text-white">
-        <input type="text" id="name" name="name" placeholder="Update Name (Optional)" class="text-white  rounded-md bg-fancyBlack w-96 mt-5 h-9 placeholder: pl-2 placeholder:text-white">
-        <input type="number" id="age" name="age" placeholder="Update Age (Optional)" class="text-white  rounded-md bg-fancyBlack w-96 mt-5 h-9 placeholder: pl-2 placeholder:text-white">
-        <input type="text" id="level" name="level" placeholder="Update Level (Optional)" class="text-white  rounded-md bg-fancyBlack w-96 mt-5 h-9 placeholder: pl-2 placeholder:text-white">
-        <input type="button" id="updateBtn" value="Update User" onclick="updateUser()" class=" w-40 mt-5 h-9 placeholder: pl-2 rounded-md bg-fancyBlack text-white text-center">
-    </form>`
-}
+
+//Reaload Page
 
 
-//Create User Function
-function createUser () { 
-const nameEl = document.getElementById('name');
-const ageEl = document.getElementById('age');
-const levelEl = document.getElementById('level');
-
-let userName = nameEl.value;
-let age = ageEl.value;
-let level = levelEl.value;
-
-
-    let body = JSON.stringify({
-
-            "name": userName,
-            "age": age,
-            "level": level
-        })
-        console.log(body)
-
-
-    fetch("https://rest-api-users.herokuapp.com/users", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-    
-        },
-        body : body
-    }) 
-        .then(result => result.json())
-        .then(data => console.log(data))
-        
-}
 
 //Delete User Function
 
-function deleteUser(){
-    const id = document.getElementById('id');
-    fetch("https://firestore.googleapis.com/v1/projects/fir-demo-99463/databases/(default)/documents/orders/" + id.value, {
+function deleteUser(orderID){
+    fetch("https://firestore.googleapis.com/v1/projects/fir-demo-99463/databases/(default)/documents/orders/" + orderID, {
         method: "DELETE"
     })
     .then(res => res.json())
     .then(data => console.log(data))
-    console.log(id.value)
+    console.log(orderID)
+
 }
+
+
+
 
 
 
 //Update User Function 
 
-function updateUser(){
+function updateUser(orderID){
     const id = document.getElementById('id');
     const nameEl = document.getElementById('name');
-    const ageEl = document.getElementById('age');
-    const levelEl = document.getElementById('level');
+    const emailEl = document.getElementById('email'); 
+    const teleEl = document.getElementById('tele');
+    const shippingMethodEl = document.getElementById('shipping-method');
+    const adressEl = document.getElementById('adress');
+    const cityEl = document.getElementById('city');
+    const postalCodeEl = document.getElementById('postal-code');
+    const orderItemsEl = document.getElementById('orderItems');
+    
+    let userName = "";
+    let email = "";
+    let tele = "";
+    let shippingMethod = "";
+    let adress = "";
+    let city = "";
+    let postalCode = "";
+    let orderItems = "";
+   
+    if(nameEl.value == ""){
+        let placeHolderValue = nameEl.getAttribute('placeholder')
+        userName = placeHolderValue
+       } else{
+         userName = nameEl.value 
+       }
 
-    let userName = nameEl.value;
-    let age = ageEl.value;
-    let level = levelEl.value;
-
-    let body = "";
-
-    if(nameEl.value == "" && levelEl.value == ""){
-          body = JSON.stringify(
-            {
-              age: age,
-            }
-        )
-    } else if (levelEl.value == "" && ageEl.value == ""){
-         body = JSON.stringify(
-            {
-              name: userName
-            }
-        )
-    } else if (ageEl.value == "" && nameEl.value== "" ){
-         body = JSON.stringify(
-            {
-              level: level
-            }
-        )
-    } else if (nameEl.value == ""){
-        body = JSON.stringify(
-            {
-              level: level,
-              age: age
-            }
-        )
-    } else if (ageEl.value == ""){
-        body = JSON.stringify(
-            {
-              name: userName,
-              level: level
-            }
-        )
-    } else if (levelEl.value == ""){
-        body = JSON.stringify(
-            {
-              name: userName,
-              age: age
-            }
-        )
-    } else {
-        body = JSON.stringify(
-            {
-              name: userName,
-              age: age,
-              level: level
-            }
-        )
-
+    if(emailEl.value == ""){
+        let placeHolderValue = emailEl.getAttribute('placeholder')
+        email = placeHolderValue
+    } else{
+        email = emailEl.value
     }
 
+    if(teleEl.value == ""){
+        let placeHolderValue = teleEl.getAttribute('placeholder')
+        tele = placeHolderValue
+    } else{
+        tele = teleEl.value
+    }
+
+    if(shippingMethodEl.value == ""){
+        let placeHolderValue = shippingMethodEl.getAttribute('placeholder')
+        shippingMethod = placeHolderValue
+    } else{
+        shippingMethod = emailEl.value
+    }
+
+    if(adressEl.value == ""){
+        let placeHolderValue = adressEl.getAttribute('placeholder')
+        adress = placeHolderValue;
+    } else{
+        adress = adressEl.value;
+    }
+
+    if(cityEl.value == ""){
+        let placeHolderValue = cityEl.getAttribute('placeholder')
+        city = placeHolderValue;
+    } else{
+        city = cityEl.value;
+    }
+
+    if(postalCodeEl.value == ""){
+        let placeHolderValue = postalCodeEl.getAttribute('placeholder')
+        postalCode = placeHolderValue
+    } else{
+        postalCode = postalCodeEl.value
+    }
+    
+    if(orderItemsEl.value == ""){
+        let placeHolderValue = orderItemsEl.getAttribute('placeholder')
+        orderItems = placeHolderValue
+    } else{
+        orderItems = orderItemsEl.value
+    }
+
+
+
+
+    const body = JSON.stringify(
+        {
+          "fields":{
+              "name":{
+                "stringValue": userName
+              } , 
+              "email":{
+                "stringValue": email
+              },
+              "number":{
+                "stringValue": tele
+              },
+              "orderItems":{
+                "stringValue": "[" + orderItems + "]"
+              },
+
+              "shippingMethod":{
+                "stringValue": shippingMethod
+              },
+              "shippingAdress": {
+                "mapValue":{
+                    "fields": {
+                        "adress": { 
+                            "stringValue": adress,
+                        },
+                        "postalCode":{
+                            "stringValue": postalCode,
+                        },
+                        "city":{
+                            "stringValue": city,
+                        }
+                    }
+                }
+            },
+             
+
+
+
+          }
+          
+          
+          
+        }
+    )
+
   
-    fetch('https://rest-api-users.herokuapp.com/users/' + id.value , {
+    fetch("https://firestore.googleapis.com/v1/projects/fir-demo-99463/databases/(default)/documents/orders/" + orderID , {
         method: 'PATCH',
         headers: {
             'Content-Type' : 'application/json'
         }, body: body
-    }) .then (result => result.json())
+    }) 
+    
+    .then (result => result.json())
         .then(data => console.log(data))
+
+
+        location.reload();
+      
 }
 
 
